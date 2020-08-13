610c2a7f7a9343da65df4c209a945b8725eeddbb
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-08-13 23:09:57 +0100

    photodialog

--- app/src/main/java/com/taofeek/cita/customer/UserEditActivity.java
+++ app/src/main/java/com/taofeek/cita/customer/UserEditActivity.java
@@ -1,18 +1,71 @@
 package com.taofeek.cita.customer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.taofeek.cita.ChangePhotoDialog;
 import com.taofeek.cita.R;
 
+import java.util.HashMap;
+import java.util.Map;
 
-public class UserEditActivity extends AppCompatActivity {
+
+public class UserEditActivity extends AppCompatActivity implements
+        ChangePhotoDialog.OnPhotoReceivedListener {
+
+    private String TAG = "testing";
+    EditText mName,mEmail,mAddress,mPhone;
+    Spinner mFacilitySpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_edit);
+        // Access a Cloud Firestore instance from your Activity
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+
+    @Override
+    public void getImagePath(Uri imagePath) {
+
+    }
+
+    @Override
+    public void getImageBitmap(Bitmap bitmap) {
 
     }
 }
