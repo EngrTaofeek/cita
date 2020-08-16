73f7df05e994833c0355ce450d35a8e774dd85b6
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-08-16 14:47:53 +0100

    failedmuploading

--- app/src/main/java/com/taofeek/cita/customer/UserEditActivity.java
+++ app/src/main/java/com/taofeek/cita/customer/UserEditActivity.java
@@ -3,21 +3,31 @@ package com.taofeek.cita.customer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 import com.taofeek.cita.ChangePhotoDialog;
 import com.taofeek.cita.R;
@@ -35,6 +45,10 @@ public class UserEditActivity extends AppCompatActivity implements
     private ImageView mImageView;
     private static final int PICK_IMAGE_REQUEST = 1;
     private Uri mImageUri;
+    private StorageTask mUploadTask;
+    private StorageReference mStorageRef;
+    private ProgressBar mProgressBar;
+
 
 
     @Override
@@ -42,6 +56,9 @@ public class UserEditActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_edit);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads/profile");
+        mName = findViewById(R.id.user_edit_name);
+        mProgressBar = findViewById(R.id.progress_bar);
         Button saveButton = findViewById(R.id.user_save_button);
         mImageView = findViewById(R.id.user_profile_pic);
         mImageView.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +72,7 @@ public class UserEditActivity extends AppCompatActivity implements
             @Override
             public void onClick(View v) {
                 addTextDocuments();
+                uploadFile();
             }
         });
 
@@ -124,6 +142,52 @@ public class UserEditActivity extends AppCompatActivity implements
                 && data != null && data.getData() != null) {
             mImageUri = data.getData();
             Picasso.get().load(mImageUri).into(mImageView);
+        }
+    }
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+    private void uploadFile() {
+        if (mImageUri != null) {
+            final StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+            mUploadTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mProgressBar.setProgress(0);
+                                }
+                            }, 500);
+                            Toast.makeText(UserEditActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
+
+
+                            Upload upload = new Upload(mName.getText().toString().trim(),
+                                    fileReference.getDownloadUrl().toString());
+//                            String uploadId = mDatabaseRef.push().getKey();
+//                            mDatabaseRef.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(UserEditActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            mProgressBar.setProgress((int) progress);
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
         }
     }
 }
