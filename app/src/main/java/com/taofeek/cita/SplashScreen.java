2ebf27ea6f06e4380d4c88469c7df14fe177d6e9
Folarin Isaac <folarinisaac36@gmail.com>
2020-08-21 17:05:03 +0100

    created a splash screen and made some changes

---
+++ app/src/main/java/com/taofeek/cita/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.taofeek.cita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    private ImageView imageView;
+    private static int splashTimeOut = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        imageView = findViewById(R.id.splash_image);
+        // splash screen to open the Main Activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splashTimeOut);
+        // create the animation
+        Animation myAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.splashanimation);
+        imageView.startAnimation(myAnim);
+    }
+}
