fe01fa14aff944ce5cddf692a89f0f34bfb30d03
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-09-04 23:13:53 +0100

    Added new designs to the guidelines

--- app/src/main/java/com/taofeek/cita/ForYouFragment.java
+++ app/src/main/java/com/taofeek/cita/ForYouFragment.java
@@ -7,6 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +30,10 @@ public class ForYouFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference detailsRef = db.collection("facility_details").
+            document("details").collection("profile");
+    private FacilityAdapter adapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -55,10 +70,38 @@ public class ForYouFragment extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_for_you, container, false);
+        View v = inflater.inflate(R.layout.fragment_for_you, container, false);
+        Query query = detailsRef.orderBy("name", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<FacilityDataModel> options = new FirestoreRecyclerOptions.Builder<FacilityDataModel>()
+                .setQuery(query, FacilityDataModel.class)
+                .build();
+
+        adapter = new FacilityAdapter(options);
+        RecyclerView recyclerView = v.findViewById(R.id.recycler_view);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        recyclerView.setAdapter(adapter);
+
+        return v;
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+
     }
 }
