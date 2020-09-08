880886cce0b05e2743f953a8fb13e206720fc97c
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-09-08 11:06:03 +0100

    Book activity retrieving data from firestore

--- app/src/main/java/com/taofeek/cita/FacilityAdapter.java
+++ app/src/main/java/com/taofeek/cita/FacilityAdapter.java
@@ -1,5 +1,9 @@
 package com.taofeek.cita;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.squareup.picasso.Picasso;
+import com.taofeek.cita.customer.BookingActivity;
 
 import static java.security.AccessController.getContext;
 
@@ -24,6 +29,7 @@ class FacilityAdapter extends FirestoreRecyclerAdapter<FacilityDataModel, Facili
      *
      * @param options
      */
+    Context mContext;
     public FacilityAdapter(@NonNull FirestoreRecyclerOptions<FacilityDataModel> options) {
         super(options);
     }
@@ -33,9 +39,11 @@ class FacilityAdapter extends FirestoreRecyclerAdapter<FacilityDataModel, Facili
         holder.name.setText(model.getName());
         holder.email.setText(model.getEmail());
         holder.address.setText(model.getAddress());
+        holder.email_text = model.getEmail();
         if(model.getImage_url() !=null){
             Picasso.get().load(model.getImage_url()).into(holder.profile);
         }
+
 
 
 
@@ -55,13 +63,28 @@ class FacilityAdapter extends FirestoreRecyclerAdapter<FacilityDataModel, Facili
         TextView address;
         ImageView profile;
         Button book_button;
-        public FacilityHolder(@NonNull View itemView) {
+        public String email_text;
+        public FacilityHolder(@NonNull final View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.list_name);
             email = itemView.findViewById(R.id.list_email);
             address = itemView.findViewById(R.id.list_address);
             profile = itemView.findViewById(R.id.list_picture);
             book_button = itemView.findViewById(R.id.list_button_book_now);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mContext = view.getContext();
+                    Intent intent = new Intent(mContext, BookingActivity.class);
+                    //intent.putExtra(BookingActivity.emailItem, email_text);
+                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("facility_item_id", email_text); //InputString: from the EditText
+                    editor.apply();
+                    mContext.startActivity(intent);
+                }
+            });
              }
     }
 }
--- app/src/main/java/com/taofeek/cita/customer/BookingActivity.java
+++ app/src/main/java/com/taofeek/cita/customer/BookingActivity.java
@@ -1,16 +1,105 @@
 package com.taofeek.cita.customer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+import com.taofeek.cita.HomeActivity;
 import com.taofeek.cita.R;
 
 public class BookingActivity extends AppCompatActivity {
+    public static final String emailItem = "facility_email";
+    public String mEmail;
+    public ImageView profile ;
+    public TextView title, address, email, phone, overview, capacity, others;
+    public Button bookNow, buttonDirection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+        //Intent intent = getIntent();
+        //mEmail = intent.getParcelableExtra(emailItem);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(BookingActivity.this);
+        final String data = prefs.getString("facility_item_id", "teslim@yahoo.com");
+        mEmail = data;
+        profile= findViewById(R.id.book_image);
+        title = findViewById(R.id.book_title);
+        address = findViewById(R.id.address_value);
+        email = findViewById(R.id.email_value);
+        phone = findViewById(R.id.phone_value);
+        overview = findViewById(R.id.overview_value);
+        capacity = findViewById(R.id.capacity_value);
+        others = findViewById(R.id.others_value);
+        populateProfilePhoto();
+        populateTextView("name",title);
+        populateTextView("address",address);
+        populateTextView("email",email);
+        populateTextView("phone",phone);
+        populateTextView("overview",overview);
+        populateTextView("capacity",capacity);
+        populateTextView("others",others);
+
+
+
     }
+    private void populateProfilePhoto() {
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("facility_details").document("details").collection("profile")
+                .document(mEmail).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    if ( document.exists()) {
+                        String field = document.getString("image_url");
+                        Picasso.get().load(field).into(profile);
+                    }
+                }
+            }
+        });
+
+
+
+        // Picasso.get().load(mImageUri).into(mImageView);
+
+    }
+    private void populateTextView(final String key, final TextView textView) {
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("facility_details").document("details").collection("profile")
+                .document(mEmail).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    if ( document.exists()) {
+                        String field = document.getString(key);
+                        textView.setText(field);
+
+                    }
+                }
+            }
+        });
+
+
+
+    }
+
 }
