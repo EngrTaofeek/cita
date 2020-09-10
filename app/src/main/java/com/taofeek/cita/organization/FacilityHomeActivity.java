fb37323a1340aa5024f94842cc5d2887300b991c
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-09-10 22:16:09 +0100

    Made few changes to the edit design and made a new app icon

--- app/src/main/java/com/taofeek/cita/organization/FacilityHomeActivity.java
+++ app/src/main/java/com/taofeek/cita/organization/FacilityHomeActivity.java
@@ -8,18 +8,33 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 import com.taofeek.cita.HomeActivity;
+import com.taofeek.cita.LoginActivity;
 import com.taofeek.cita.R;
 import com.taofeek.cita.customer.UserEditActivity;
 import com.taofeek.cita.organization.main.SectionsPagerAdapter;
 
 public class FacilityHomeActivity extends AppCompatActivity {
+    public ImageView mImageView;
+    public TextView mNameTextView;
+    public TextView mEmailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,24 +56,84 @@ public class FacilityHomeActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs_facility);
         tabs.setupWithViewPager(viewPager);
+        View hView =  navigationView.getHeaderView(0);
+        mImageView = hView.findViewById(R.id.nav_image_view);
+        mNameTextView = hView.findViewById(R.id.nav_header_title);
+        mEmailTextView = hView.findViewById(R.id.nav_email);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 switch (id) {
                     case R.id.nav_schedule:
-                        Snackbar.make(findViewById(R.id.nav_view), "Donate", Snackbar.LENGTH_LONG).show();
+                        Snackbar.make(findViewById(R.id.nav_view), "Schedule is upcoming", Snackbar.LENGTH_LONG).show();
                         break;
+
                     case R.id.nav_edit_profile:
                         Intent editIntent = new Intent(FacilityHomeActivity.this, FacilityEditActivity.class);
                         startActivity(editIntent);
-
                         break;
+
+                        case R.id.nav_switch_to_user:
+                        Intent switchUserIntent = new Intent(FacilityHomeActivity.this, HomeActivity.class);
+                        startActivity(switchUserIntent);
+                        break;
+
+                    case R.id.nav_log_out:
+                        FirebaseAuth.getInstance().signOut();
+                        Intent signOutIntent = new Intent(FacilityHomeActivity.this, LoginActivity.class);
+                        startActivity(signOutIntent);
+                        break;
+
                     default:
                         return true;
                 }
                 return true;
             }
         });
+    }
+    private void retrieveNavHeader() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(FacilityHomeActivity.this);
+        final String data = prefs.getString("email_id", "default_email");
+
+        mEmailTextView.setText(data);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users").document("details").collection("profile")
+                .document(data).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    if ( document.exists()) {
+                        String field = document.getString("image_url");
+                        Picasso.get().load(field).into(mImageView);
+                    }
+                }
+            }
+        });db.collection("users").document("details").collection("profile")
+                .document(data).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if ( document.exists()) {
+                                String field = document.getString("name");
+                                mNameTextView.setText(field);
+                            }
+                        }
+                    }
+                });
+
+
+
+        // Picasso.get().load(mImageUri).into(mImageView);
+
+    }
+    @Override
+    protected void onStart() {
+        retrieveNavHeader();
+
+        super.onStart();
     }
 }
