84fe84d3020a516ab8e7b408299f4af023002009
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-09-28 08:38:04 +0100

    Google Sign In implemented IN RegisterActivity

--- app/src/main/java/com/taofeek/cita/RegisterActivity.java
+++ app/src/main/java/com/taofeek/cita/RegisterActivity.java
@@ -4,20 +4,31 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.taofeek.cita.organization.FacilityHomeActivity;
 
 import static android.text.TextUtils.isEmpty;
 
@@ -31,6 +42,10 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mEmail, mPassword, mConfirmPassword;
     private Button mRegister;
     private ProgressBar mProgressBar;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 9001;
+    private CheckBox mCheckBox;
 
 
     @Override
@@ -46,6 +61,8 @@ public class RegisterActivity extends AppCompatActivity {
         mConfirmPassword = (EditText) findViewById(R.id.confirm_password_input);
         mRegister = (Button) findViewById(R.id.button);
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
+        mCheckBox = findViewById(R.id.checkbox);
         hideDialog();
 
         mRegister.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +93,15 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         hideSoftKeyboard();
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+        preGoogleSignInSetUp();
 
     }
     public void registerNewEmail(final String email, String password){
@@ -144,21 +170,91 @@ public class RegisterActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-    /**
-     * Return true if @param 's1' matches @param 's2'
-     * @param s1
-     * @param s2
-     * @return
-     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        // updateUI(account);
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    public void preGoogleSignInSetUp(){
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // Set the dimensions of the sign-in button.
+
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(RegisterActivity.this);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String email = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+                FirebaseUser user = mAuth.getCurrentUser();
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(RegisterActivity.this);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("email_id", email); //InputString: from the EditText
+                editor.apply();
+            }
+
+
+            Intent user_intent = new Intent(RegisterActivity.this, HomeActivity.class);
+            Intent facility_intent = new Intent(RegisterActivity.this, FacilityHomeActivity.class);
+            if (mCheckBox.isChecked()){
+                hideDialog();
+                startActivity(facility_intent);
+                finish();
+            }
+            else {
+                hideDialog();
+                startActivity(user_intent);
+                finish();
+
+            }
+
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+
+        }
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
     private boolean doStringsMatch(String s1, String s2){
         return s1.equals(s2);
     }
 
-    /**
-     * Return true if the @param is null
-     * @param string
-     * @return
-     */
     private boolean isEmpty(String string){
         return string.equals("");
     }
