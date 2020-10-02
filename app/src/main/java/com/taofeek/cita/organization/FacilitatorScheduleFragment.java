49c950b724b49f9d4585b12d68ce0f94330276b3
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-10-03 00:00:01 +0100

    Schedule

--- app/src/main/java/com/taofeek/cita/organization/FacilitatorScheduleFragment.java
+++ app/src/main/java/com/taofeek/cita/organization/FacilitatorScheduleFragment.java
@@ -3,18 +3,23 @@ package com.taofeek.cita.organization;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import com.google.firebase.firestore.Query;
+
+import com.taofeek.cita.FacilityDataModel;
 import com.taofeek.cita.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FacilitatorScheduleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FacilitatorScheduleFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -25,20 +30,16 @@ public class FacilitatorScheduleFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference detailsRef = db.collection("facility_details")
+            .document("details").collection("appointment");
+    private ScheduleAdapter adapter;
 
     public FacilitatorScheduleFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FacilitatorScheduleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FacilitatorScheduleFragment newInstance(String param1, String param2) {
         FacilitatorScheduleFragment fragment = new FacilitatorScheduleFragment();
         Bundle args = new Bundle();
@@ -60,8 +61,34 @@ public class FacilitatorScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_facilitator_schedule, container, false);
+        View v = inflater.inflate(R.layout.fragment_facilitator_schedule, container, false);
+        Query query = detailsRef.orderBy("name", Query.Direction.ASCENDING);
+
+        FirestoreRecyclerOptions<ScheduleDataModel> options = new FirestoreRecyclerOptions.Builder<ScheduleDataModel>()
+                .setQuery(query, ScheduleDataModel.class)
+                .build();
+
+        adapter = new ScheduleAdapter(options);
+        RecyclerView recyclerView = v.findViewById(R.id.recycler_schedule);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        recyclerView.setAdapter(adapter);
+
+        return v;
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
 
     }
 }
--- app/src/main/java/com/taofeek/cita/organization/ScheduleDataModel.java
+++ app/src/main/java/com/taofeek/cita/organization/ScheduleDataModel.java
@@ -1,7 +1,9 @@
 package com.taofeek.cita.organization;
 
 public class ScheduleDataModel {
-    private String date, time, email;
+
+
+    private String name, date, time, email;
 
     public String getDate() {
         return date;
@@ -20,10 +22,14 @@ public class ScheduleDataModel {
         //EMPTY CONSTRUCTOR
 
     }
-    public ScheduleDataModel(String date, String time, String email){
+    public String getName() {
+        return name;
+    }
+    public ScheduleDataModel(String date, String time, String email , String name){
         this.date = date;
         this.time = time;
         this.email = email;
+        this.name = name;
     }
 
 
