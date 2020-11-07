52eb34b17d940603e50cfc47ca22986b1356dc59
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-11-07 01:52:19 +0100

    Playstore version

--- app/src/main/java/com/taofeek/citaa/MainActivity.java
+++ app/src/main/java/com/taofeek/citaa/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.messaging.RemoteMessage;
 
 
 public class MainActivity extends AppCompatActivity {
